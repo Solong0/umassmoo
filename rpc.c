106842beb01ebea2ae097e5303093ab89218c74f
Ben Ransford <ransford@cs.umass.edu>
2011-07-12 13:22:13 -0400

    temperature-sensor bugfix from Hong
    fixes this problem: "I found some bugs in the temperature code. The temperature
    sensor chip isn't powered on/off."

--- rpc.c
+++ rpc.c
@@ -63,21 +63,28 @@ unsigned int rpc_kill (void) {
 };
 
 unsigned int rpc_get_temperature (void) {
-    // XXX copied from (sufficiently tested?) tests/sensorTest.c
-    unsigned int adcval;
+    unsigned int i, adcval;
 
-    // setup ADC for external temperature sensor
+    // setup ADC to read external analog temperature sensor
     ADC12CTL0 &= ~ENC; // make sure this is off otherwise settings are locked.
-    P6SEL |= INCH_TEMP_EXT_IN;                      // Enable A/D channel A4
+    P6SEL |= TEMP_EXT_IN;                      // Enable A/D channel A4
     ADC12CTL0 = ADC12ON + SHT0_2 + REFON + REF2_5V; // Turn on and set up ADC12
     ADC12CTL1 = SHP;                                // Use sampling timer
     ADC12MCTL0 = INCH_TEMP_EXT_IN + SREF_1;         // Vr+=Vref+
+
+    // turn on temperature sensor
+    P1DIR |= TEMP_POWER;
+    P1OUT |= TEMP_POWER;
+
+    // XXX should sleep instead?
+    for (i = 0; i != 150; ++i); // allow sensor time to stabilize
 
     ADC12CTL0 |= ENC | ADC12SC; // enable and start conversion
     _BIC_SR(GIE); // disable interrupts while busy-waiting on ADC
     while (ADC12CTL1 & ADC12BUSY); // busy-wait for ADC to sample
     adcval = ADC12MEM0;
     ADC12CTL0 = ADC12CTL1 = 0; // turn off ADC
+    P1OUT &= ~TEMP_POWER; // turn off temperature sensor
     _BIS_SR(GIE); // reenable interrupts
 
     return adcval;
