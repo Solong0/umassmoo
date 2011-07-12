3b322f5b8733c2c690a64ddd40e3ffd8bfbc3a37
Andres Molina-Markham <amolina@cs.umass.edu>
2011-07-11 23:47:26 -0400

    OK

--- kill.c
+++ kill.c
@@ -4,22 +4,19 @@
 #include "flash.h"
 
 unsigned char kill (void) {
-<<<<<<< HEAD
     while (!is_killed())
     {
       extflash_write_uchar((unsigned long)KILLSWITCH_ADDR, 1);
     }
-    return 0;
-=======
-    while (! is_killed())
-        extflash_write_uchar((unsigned long)KILLSWITCH_ADDR, 1);
     return 1;
+    //while (! is_killed())
+    //    extflash_write_uchar((unsigned long)KILLSWITCH_ADDR, 1);
+    //return 1;
 }
 
 unsigned char unkill (void) {
     extflash_erase_segment((unsigned long)KILLSWITCH_ADDR);
     return 1;
->>>>>>> f8698b121946ff0ca299fea3b342bb7df606531f
 }
 
 unsigned char is_killed (void) {
